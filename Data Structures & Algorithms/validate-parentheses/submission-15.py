500d2638a4a5071977702daec1115cb32177d74a
vaibhav1096 <54505144+vaibhav1096@users.noreply.github.com>
2026-05-05 18:21:41 -0700

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dictn={
+            '}':'{',
+            ')':'(',
+            ']':'['
+        }
+        stack=[]
+        for i in s:
+            if i in dictn.keys():
+                if stack and stack[-1]==dictn[i]  :
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+                print(stack)
+        return False if stack else True
+            
